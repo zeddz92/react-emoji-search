ee2a78bbd0960fac184ffcdc6581ba85a07250f8
Edd <reyes@agilemedia.jp>
2021-12-29 19:47:12 +0900

    Update EmojiPicker sttories

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,30 +1,52 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import { Meta, Story } from "@storybook/react";
-import Component from "./index";
+import { EmojiPicker, Emoji } from "./index";
+import { EmojiSet } from "types/emoji";
 
 const meta: Meta<PropsWithChildren<{}>> = {
   title: "My Component",
-  component: Component,
+  component: EmojiPicker,
   argTypes: {
-    children: {
-      description: "Content or elements to be rendered inside the Component",
+    set: {
+      defaultValue: "facebook",
       control: {
-        type: "text",
+        type: "select",
+        labels: {
+          native: "Native",
+          apple: "Apple",
+          google: "Google",
+          facebook: "Facebook",
+          twitter: "Twitter",
+        },
+      },
+    },
+    mode: { defaultValue: "dark" },
+    quality: { defaultValue: "clean" },
+    styles: {
+      defaultValue: {
+        backgroundColor: "...",
+        indicatorColor: "...",
+        fontColor: "...",
+        tabsFontColor: "...",
+        searchFontColor: "...",
+        variationPickerBackgroundColor: "...",
       },
     },
   },
 };
 
-const Template: Story<PropsWithChildren<{}>> = (args) => (
-  <div style={{ height: 400 }}>
-    <Component {...args} />
-  </div>
-);
+const Template: Story<PropsWithChildren<{ set: EmojiSet }>> = (args) => {
+  return (
+    <>
+      <div style={{ height: 500 }}>
+        <EmojiPicker {...args} tabsVariant="fullWidth" mode="dark" />
+      </div>
+    </>
+  );
+};
 
 const Basic = Template.bind({});
-Basic.args = {
-  children: "Component",
-};
+Basic.args = {};
 
 export default meta;
 export { Basic };
