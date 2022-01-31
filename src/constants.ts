72a5fe6be87684db35af49e7311fc77d74c20782
Edd <reyes@agilemedia.jp>
2022-01-31 17:55:08 +0900

    Add map categories

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,14 @@
 export const LOCAL_STORAGE_RECENT = "react-emoji-search-recent";
 export const LOCAL_STORAGE_VARIATION = "react-emoji-search-variations";
+
+export const categories: { [key: string]: string } = {
+  Recent: "recent",
+  "Smileys & People": "smileys-people",
+  "Animals & Nature": "animals-nature",
+  "Food & Drink": "food-drink",
+  Activity: "activity",
+  "Travel & Places": "travel-places",
+  Objects: "objects",
+  Symbols: "symbols",
+  Flags: "flags",
+};
