18a0990ec5243b44131f52b085f6a6e515b5c420
Edd <reyes@agilemedia.jp>
2022-01-31 17:39:52 +0900

    map css files

--- jest.config.js
+++ jest.config.js
@@ -5,41 +5,28 @@
 
 module.exports = {
   // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    "node_modules",
-    "src"
-  ],
+  moduleDirectories: ["node_modules", "src"],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "ts",
-    "tsx",
-    "json",
-    "node"
-  ],
+  moduleFileExtensions: ["js", "ts", "tsx", "json", "node"],
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    "src"
-  ],
+  roots: ["src"],
 
   // The test environment that will be used for testing
   testEnvironment: "jest-environment-jsdom",
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "\\\\node_modules\\\\"
-  ],
+  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js",
+  },
 };
