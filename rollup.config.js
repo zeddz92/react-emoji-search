c7959f39cd02093bfddc3a4be4c73557173fba99
Edd <reyes@agilemedia.jp>
2022-01-31 18:45:07 +0900

    Update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,71 @@
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+import esBuild, { minify } from "rollup-plugin-esbuild";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
-
-// Array of extensions to be handled by babel
-const EXTENSIONS = [".ts", ".tsx"];
 
 // Excluded dependencies
 const devExternal = Object.keys(pkg.devDependencies);
 const external = Object.keys(pkg.dependencies);
 
-export default {
-  input: ["src/index.tsx"],
-  output: {
-    dir: "dist",
-    sourcemap: true,
-    format: "esm",
-    preserveModules: true,
-  },
-  plugins: [
-    peerDepsExternal(),
-    commonjs(),
-    resolve({
-      mainFields: ["module", "main", "jsnext:main", "browser"],
-      extensions: [...EXTENSIONS, ".css"],
-    }),
-    babel({
-      extensions: EXTENSIONS,
-      exclude: "./node_modules/**",
-    }),
-    postcss({ minimize: true }),
-    {
-      name: "Custom Rollup Plugin`",
+const bundle = (config) => ({
+  input: "src/index.ts",
+  external: [...devExternal, ...external, /\.js$/],
+  ...config,
+});
 
-      generateBundle: (_, bundle) => {
-        Object.entries(bundle).forEach((entry) => {
-          bundle[entry[0]].code = entry[1].code.replace(
-            "../../node_modules/",
-            ""
-          );
-          bundle[entry[0]].code = entry[1].code.replace("../node_modules/", "");
-        });
+export default [
+  bundle({
+    plugins: [
+      esBuild({ minify: true }),
+      postcss({
+        minimize: true,
+        inject: true,
+      }),
+      {
+        name: "Use style-inject commonJS module`",
+        generateBundle: (_, bundle) => {
+          Object.entries(bundle).forEach((entry) => {
+            bundle[entry[0]].code = entry[1].code.replace(
+              "style-inject.es.js",
+              "style-inject.js"
+            );
+            bundle[entry[0]].code = entry[1].code.replace(
+              "../node_modules/",
+              ""
+            );
+          });
+        },
       },
+    ],
+    output: [
+      {
+        file: "dist/index.js",
+        sourcemap: true,
+        format: "cjs",
+      },
+      {
+        file: "dist/index.mjs",
+        sourcemap: true,
+        format: "es",
+      },
+    ],
+  }),
+  {
+    input: "src/data/emojis.js",
+    plugins: [minify()],
+    output: {
+      exports: "auto",
+      dir: "dist/data",
+      format: "cjs",
     },
-    terser(),
-  ],
-  external: [...devExternal, ...external],
-};
+  },
+  bundle({
+    plugins: [dts()],
+    external: [/types\//, /\.js$/, /\.css$/],
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+  }),
+];
